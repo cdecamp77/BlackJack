dab44711e6b7d2cece26b0c20a67f6fdd705965a
Christopher De Camp <cdecamp77@gmail.com>
2017-10-05 18:40:55 -0700

    First Commit

--- js/main.js
+++ js/main.js
@@ -6,24 +6,15 @@ var bank = "$" + 1000;
 var wager = "$" + 100;
 
 
-$(function(){
-  /*----- constants -----*/
-var cards = [
-    {'ace11': 11,
-    'ace1': 1,
-     'king' : 10,
-     'queen': 10,
-     'jack': 10,
-     'ten': 10,
-     'nine': 9,
-     'eight': 8,
-     'seven': 7,
-     'six': 6,
-     'five': 5,
-     'four': 4,
-     'three': 5,
-     'two': 2,
-    }];
+
+
+/*----- constants -----*/
+var deckNames = [
+  "sA", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "sJ", "sQ", "sK",
+  "hA", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9", "h10", "hJ", "hQ", "hK",
+  "cA", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "c10", "cJ", "cQ", "cK",
+  "dA", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "d10", "dJ", "dQ", "dK",
+];
 
 
 
@@ -37,41 +28,39 @@ var cards = [
 
 
 /*----- cached element references -----*/
-
-var playerTotal = 7;
-var dealerTotal = 7;
-function aceP (playerTotal) {
+// if player gets an ace
+function aceP(playerTotal) {
   if (playerTotal + 11 === 21) {
     //WIN if playerTotal > dealerTotal
     //PUSH if playerTotal === dealerTotal
-    console.log(playerTotal);
+    console.log("21");
   } else if (playerTotal + 11 <= 20) {
-  playerTotal = playerTotal + 11;
-  // option to hit or stay 
-      console.log(playerTotal);
-} else {
-  playerTotal = playerTotal + 1;
-  // option to hit or stay
-      console.log(playerTotal);
-}
+    playerTotal = playerTotal + 11;
+    // option to hit or stay 
+    console.log('ace becomes 11' , playerTotal);
+  } else {
+    playerTotal = playerTotal + 1;
+    // option to hit or stay
+    console.log('ace is 1',playerTotal);
+  }
 }
 
-
-function aceD (dealerTotal) {
+// if dealer gets an ace
+function aceD(dealerTotal) {
   if (dealerTotal + 11 === 21 && dealerTotal + 11 >= 18) {
     //Dealer stays
     console.log(dealerTotal);
   } else if (dealerTotal + 11 === 17) {
     dealerTotal = dealerTotal + 11;
     // dealer hits
-        console.log(dealerTotal);
+    console.log(dealerTotal);
   } else if (dealerTotal + 11 <= 16) {
     dealerTotal = dealerTotal + 11;
     //dealer hits
-        console.log(dealerTotal);
-  } else { 
+    console.log(dealerTotal);
+  } else {
     dealerTotal = dealerTotal + 1;
-        console.log(dealerTotal);
+    console.log(dealerTotal);
   }
 }
 
@@ -80,25 +69,49 @@ function aceD (dealerTotal) {
 
 
 
+
 /*----- event listeners -----*/
-$('.deal').on('click', function(deal) {
-    console.log('deal')
+$(function () {
+
+  // var deal = function (deck) {
+  // // var card = deck.splice(Math.floor(Math.random() * deck.length), 1);
+  // playerHand =  deck.splice(Math.floor(Math.random() * deck.length), 1);
+  // dealerHand = deck.splice(Math.floor(Math.random() * deck.length), 1);
+  // }
+
+  $('.deal').on('click', function (deal) {
+    console.log(playerHand.length)
+    if (playerHand.length == 0) {
+
+      playerHand.push(dealRandomCard());
+      playerHand.push(dealRandomCard());
+
+      dealerHand.push(dealRandomCard());
+      dealerHand.push(dealRandomCard());
+    }
+
+    console.log(playerHand);
+    console.log(dealerHand);
+
+
+
     // Store Bet amount in a VAR
     //dealer receives 1 card face up and 
     // one card face down.
     // player receives 2 cards face up
-});
+  });
 
-$('.hit').on('click', function(){
-    console.log('hit')
+  $('.hit').on('click', function () {
+    playerHand.push(dealRandomCard());
+    console.log(playerHand)
     // adds one card to the players hand and
     // add the value to the 1st 2 cards
     // if total is <= 21 keep playing...
     // Else you lose, subtract Bet from 
     // players Bank
-});
+  });
 
-$('.stay').on('click', function(stay) {
+  $('.stay').on('click', function (stay) {
     console.log('stay')
     // player has opted to stay with 
     // cards value.
@@ -111,8 +124,9 @@ $('.stay').on('click', function(stay) {
     // PUSH
     // If dealerHand < playerHand ....
     // Player wins, add Bet to Bank
-});
+  });
 
+});
 
 
 
@@ -120,6 +134,10 @@ $('.stay').on('click', function(stay) {
 
 /*----- functions -----*/
 
+//making a wager
+// Set the initial value of Wager ie. $100;
 
 
-});
+function dealRandomCard() {
+  return deckNames.splice(Math.floor(Math.random() * deckNames.length), 1)[0];
+}
