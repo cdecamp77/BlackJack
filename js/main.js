3847ecf5aa49f510dbb58c3bc03f390652ed879c
Christopher De Camp <cdecamp77@gmail.com>
2017-10-06 20:37:18 -0700

    Added message and total to display, still needs tweaking

--- js/main.js
+++ js/main.js
@@ -32,116 +32,60 @@ Deck.names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 Deck.values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11];
 Deck.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
 
-/*----- cached element references -----*/
-// if player gets an ace
-// function aceP(playerTotal) {
-//   if (playerTotal + 11 === 21) {
-//     //WIN if playerTotal > dealerTotal
-//     //PUSH if playerTotal === dealerTotal
-//     console.log("21");
-//   } else if (playerTotal + 11 <= 20) {
-//     playerTotal = playerTotal + 11;
-//     // option to hit or stay 
-//     console.log('ace becomes 11', playerTotal);
-//   } else {
-//     playerTotal = playerTotal + 1;
-//     // option to hit or stay
-//     console.log('ace is 1', playerTotal);
-//   }
-// }
+;
 
-// if dealer gets an ace
-// function aceD(dealerTotal) {
-//   if (dealerTotal + 11 === 21 && dealerTotal + 11 >= 18) {
-//     console.log("Dealer stays", dealerTotal);
-//   } else if (dealerTotal + 11 === 17) {
-//     dealerTotal = dealerTotal + 11;
-//     dealerHand(dealRandomCard());
-//     console.log("dealer hits, soft 17", dealerTotal);
-//   } else if (dealerTotal + 11 <= 16) {
-//     dealerTotal = dealerTotal + 11;
-//     dealerHand(dealRandomCard());
-//     console.log("dealer hits <=16", dealerTotal);
-//   } else {
-//     dealerTotal = dealerTotal + 1;
-//     dealerHand(dealRandomCard());
-//     console.log("dealer ace 1", dealerTotal);
-//   }
-// }
 
 /*----- event listeners -----*/
 $(function () {
-
-  // var deal = function (deck) {
-  // // var card = deck.splice(Math.floor(Math.random() * deck.length), 1);
-  // playerHand =  deck.splice(Math.floor(Math.random() * deck.length), 1);
-  // dealerHand = deck.splice(Math.floor(Math.random() * deck.length), 1);
-  // }
-
   $('.deal').on('click', function () {
 
     deal();
     var playerTotal = computeHand(playerHand);
     var dealerTotal = computeHand(dealerHand);
+    messTotal();
     if (dealerTotal === 21 && playerTotal != 21) {
-        console.log('21, dealer wins', playerTotal)
+      setMessage('21, dealer wins')
     }
-    console.log(computeHand(playerHand))
-    console.log(computeHand(dealerHand))
+    // setMessage(computeHand(playerHand))
+    // setMessage(computeHand(dealerHand))
     // Store Bet amount in a VAR
-    //dealer receives 1 card face up and 
-    // one card face down.
-    // player receives 2 cards face up
-  });
+});
 
   $('.hit').on('click', function () {
     playerHand.push(dealRandomCard());
     var playerTotal = computeHand(playerHand);
     var dealerTotal = computeHand(dealerHand);
+    messTotal();
     if (playerTotal > 21 && dealerTotal < 22) {
-      console.log('Bust, player loses');
+      setMessage('Bust, player loses');
     } else if (playerTotal > 21) {
-        console.log('bust, player loses', playerTotal);
+      setMessage('bust, player loses');
     }
-    console.log(computeHand(playerHand))
-    console.log(computeHand(dealerHand))
-    // adds one card to the players hand and
-    // add the value to the 1st 2 cards
-    // if total is <= 21 keep playing...
-    // Else you lose, subtract Bet from 
-    // players Bank
+    // setMessage(computeHand(playerHand))
+    // setMessage(computeHand(dealerHand))
+    // Else you lose, subtract Bet from players Bank
   });
 
   $('.stay').on('click', function () {
-      playerTotal = computeHand(playerHand)
+    playerTotal = computeHand(playerHand);
+    dealerTotal = computeHand(dealerHand);
+    messTotal();
+    while (dealerTotal <= 16) {
+      dealerHand.push(dealRandomCard());
       dealerTotal = computeHand(dealerHand);
-      while (dealerTotal <= 16) {
-          dealerHand.push(dealRandomCard());
-          dealerTotal = computeHand(dealerHand);  
-          if (dealerTotal > 21) {
-              console.log('dealer busts', dealerTotal);
-          }  
+      messTotal();
+      if (dealerTotal > 21) {
+        setMessage('dealer busts');
       }
-
-    if (dealerTotal === playerTotal) {
-      console.log('push')
-    } 
-     else if (dealerTotal > playerTotal && dealerTotal < 22) {
-      console.log('Dealer wins', dealerTotal, playerTotal)
-    } else {
-      console.log('player wins', dealerTotal, playerTotal);
     }
-
-    // player has opted to stay with 
-    // cards value.
-    // turn goes to the Dealer.
-    // Dealer hits until their total is
-    // >= 17.
-    // If dealerHand > playerHand...
+    if (dealerTotal === playerTotal) {
+      setMessage('push')
+    } else if (dealerTotal > playerTotal && dealerTotal < 22) {
+      setMessage('Dealer wins');
+    } else {
+      setMessage('player wins');
+    }
     // player loses. Subt Bet from Bank
-    //If dealerHand = playerHand...
-    // PUSH
-    // If dealerHand < playerHand ....
     // Player wins, add Bet to Bank
   });
 });
@@ -152,39 +96,55 @@ $(function () {
 function dealRandomCard() {
   return deck.cards.splice(Math.floor(Math.random() * deck.cards.length), 1)[0];
 }
-
+// deals the initial cards to the player and dealer
 function deal() {
-    if (playerHand.length == 0) {
-      playerHand.push(dealRandomCard());
-      dealerHand.push(dealRandomCard());
-      playerHand.push(dealRandomCard());
-      dealerHand.push(dealRandomCard());
-    } 
-    // else if (dealerTotal === 21 && playerTotal != 21) {
-    //     console.log('Dealer BJ');
-    //     deal();
-     else {
-        playerHand = [];
-        dealerHand = [];
-        deal();
-    }
+  if (playerHand.length == 0) {
+    playerHand.push(dealRandomCard());
+    dealerHand.push(dealRandomCard());
+    playerHand.push(dealRandomCard());
+    dealerHand.push(dealRandomCard());
+  } else {
+    playerHand = [];
+    dealerHand = [];
+    deal();
+  }
+messTotal();
 }
 
-
+//computes hands for both players, taking acct for aces
 function computeHand(hand) {
-  //return the best hand
   var aceCount = 0;
-  var total = hand.reduce(function(acc, card) {
+  var total = hand.reduce(function (acc, card) {
     if (card.value === 11) aceCount++;
     return acc + card.value;
   }, 0);
-  while(total > 21 && aceCount) {
-      total -= 10;
-      aceCount--;
+  while (total > 21 && aceCount) {
+    total -= 10;
+    aceCount--;
   }
   return total;
 }
 
+// reshuffle deck at certain point, playing out current hand
+function reshuffle() {
+    if (Deck.length <= 15) {
+        // play current hands and reshuffle
+        deal();
+    }
+}
+
+// displays the totals for user
+function messTotal() {
+    var playerTotal = computeHand(playerHand);
+    var dealerTotal = computeHand(dealerHand);
+    document.querySelector('.pTotal').innerHTML = "Player total is " + playerTotal;
+    document.querySelector('.dTotal').innerHTML = "Dealer total is " + dealerTotal;
+}
+
+//changes the main message
+function setMessage(message) {
+    document.querySelector('.message').innerHTML = message;
+}
 
 
 deck.createAllCards();
